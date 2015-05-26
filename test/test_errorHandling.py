7d056a4252bc2722773545255fa00689ea1805e6
Thomas Lavergne <thomas.lavergne@met.no>
2015-05-26 22:36:10 +0200

    Fix the error handling with PLL runs

--- test/test_errorHandling.py
+++ test/test_errorHandling.py
@@ -15,26 +15,42 @@ class Test_DieOnError(unittest.TestCase):
        We use script ./doer1 that barks on one date
     """
 
-#    def test_DoE_PLL_woutFlag(self):
-#        """ Test pll run without --die-on-error """
-#        cmd = [exe,'--cpu all','--quiet','20100225','20100310',doer,'{d:%Y%m%d}']
-#        p = Popen(cmd,stdout=PIPE,stderr=STDOUT)
-#        out, err = p.communicate()
-#        ok_run = 0
-#        fail_run = 0
-#        for l in out.splitlines():
-#            if l.startswith('process '):
-#                ok_run += 1
-#            elif l.startswith('ERROR:'):
-#                fail_run += 1
-#
-#        print cmd
-#        print out
-#
-#        self.assertEqual((ok_run+fail_run),14,msg='Did not spawn expected number of "doer1" runs')
-#        self.assertEqual(ok_run,13,msg='Did not receive expected number of SUCCESS "doer1" runs')
-#        self.assertEqual(fail_run,1,msg='Did not receive expected number of ERROR "doer1" runs')
-#        self.assertNotEqual(p.returncode,0,msg='This run should have returned non-zero return code (got {})'.format(p.returncode,))
+
+    def test_DoE_PLL_withFlag(self):
+        """ Test pll run with --die-on-error """
+        cmd = exe + ' --die-on-error --cpu 2 --quiet 20100225 20100310 '+ doer + ' {d:%Y%m%d}'
+        p = Popen(cmd,stdout=PIPE,stderr=STDOUT,shell=True)
+        out, err = p.communicate()
+        ok_run = 0
+        fail_run = 0
+        for l in out.splitlines():
+            if l.startswith('process '):
+                ok_run += 1
+            elif l.startswith('ERROR:'):
+                fail_run += 1
+
+        self.assertEqual((ok_run+fail_run),9,msg='Did not spawn expected number of "doer1" runs')
+        self.assertEqual(ok_run,8,msg='Did not receive expected number of SUCCESS "doer1" runs')
+        self.assertEqual(fail_run,1,msg='Did not receive expected number of ERROR "doer1" runs')
+        self.assertNotEqual(p.returncode,0,msg='This run should have returned non-zero return code (got {})'.format(p.returncode,))
+
+    def test_DoE_PLL_woutFlag(self):
+        """ Test pll run without --die-on-error """
+        cmd = exe + ' --cpu all --quiet 20100225 20100310 '+ doer + ' {d:%Y%m%d}'
+        p = Popen(cmd,stdout=PIPE,stderr=STDOUT,shell=True)
+        out, err = p.communicate()
+        ok_run = 0
+        fail_run = 0
+        for l in out.splitlines():
+            if l.startswith('process '):
+                ok_run += 1
+            elif l.startswith('ERROR:'):
+                fail_run += 1
+
+        self.assertEqual((ok_run+fail_run),14,msg='Did not spawn expected number of "doer1" runs')
+        self.assertEqual(ok_run,13,msg='Did not receive expected number of SUCCESS "doer1" runs')
+        self.assertEqual(fail_run,1,msg='Did not receive expected number of ERROR "doer1" runs')
+        self.assertNotEqual(p.returncode,0,msg='This run should have returned non-zero return code (got {})'.format(p.returncode,))
 
     def test_DoE_serial_withFlag(self):
         """ Test serial run with --die-on-error """
