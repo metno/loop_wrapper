7d00b1f02bfd95dba2c7106e16ac3d30507a5e24
Thomas Lavergne <thomas.lavergne@met.no>
2015-05-28 21:33:52 +0200

    API change, the shell expansion constructs must now be inside []. The previous API gave too much freedom, and there was too many ways to make mistakes. This triggers 3.0.0.

--- test/test_shellExpansion.py
+++ test/test_shellExpansion.py
@@ -15,9 +15,30 @@ doer3 = os.path.join(os.path.realpath(os.path.dirname(__file__)),'doer3')
 class Test_ShellExpansion_wF(unittest.TestCase):
     """ Test the Shell Parameter Expansion, with {f:} reference """
 
+    def test_shell_expansion_wF_nowildcard(self):
+        """ Test combining date looping, no wildcard and {F:} referencing """
+        cmd = [exe,'--quiet','20051228','20060102',doer3,
+               '['+os.path.join(tst_dir,'{d:%Y}','tst_A_{d:%Y%m%d}.tar')+']','/tmp/{F:}.gz']
+        out = check_output(cmd)
+        lines = out.splitlines()
+        print len(lines),lines
+        self.assertEqual(len(lines),6,msg='Basic substitution with {F:} failed when no wildcards are used')
+        # Test the {F:} substitution worked as expected
+        ok_lines = 0
+        for l in lines:
+            if l.startswith('process '):
+                from_file = (l.split())[1]
+                to_file   = (l.split())[-1]
+                if from_file.endswith('.tar') and os.path.exists(from_file) and \
+                   to_file == os.path.join('/tmp',os.path.basename(from_file))+'.gz':
+                    ok_lines += 1
+
+        self.assertEqual(ok_lines,6,msg='Filename substitution with {F:} failed when no wildcards are used')
+
     def test_shell_expansion_wF(self):
         """ Test combining date looping, shell parameter expansion  (with '?') and {F:} referencing """
-        cmd = [exe,'--quiet','20051228','20060102',doer3,os.path.join(tst_dir,'{d:%Y}','tst_?_{d:%Y%m%d}.tar'),'/tmp/{F:}.gz']
+        cmd = [exe,'--quiet','20051228','20060102',doer3,
+               '['+os.path.join(tst_dir,'{d:%Y}','tst_?_{d:%Y%m%d}.tar')+']','/tmp/{F:}.gz']
         out = check_output(cmd)
         lines = out.splitlines()
         self.assertEqual(len(lines),18,msg='Basic shell expansion with "?" failed')
@@ -35,7 +56,8 @@ class Test_ShellExpansion_wF(unittest.TestCase):
 
     def test_shell_expansion_wf(self):
         """ Test combining date looping, shell parameter expansion  (with '?') and {f:} referencing """
-        cmd = [exe,'--quiet','20051228','20060102',doer3,os.path.join(tst_dir,'{d:%Y}','tst_?_{d:%Y%m%d}.tar'),'{f:}.gz']
+        cmd = [exe,'--quiet','20051228','20060102',doer3,
+               '['+os.path.join(tst_dir,'{d:%Y}','tst_?_{d:%Y%m%d}.tar')+']','{f:}.gz']
         out = check_output(cmd)
         lines = out.splitlines()
         self.assertEqual(len(lines),18,msg='Basic shell expansion with "?" failed')
@@ -64,7 +86,8 @@ class Test_ShellExpansion_noF(unittest.TestCase):
 
     def test_shell_expansion_range_noF(self):
         """ Test combining date looping and shell parameter expansion  (with character ranges)"""
-        cmd = [exe,'--quiet','20051228','20060102',doer2,os.path.join(tst_dir,'{d:%Y}','tst_[A:B]_{d:%Y%m%d}.tar')]
+        cmd = [exe,'--quiet','20051228','20060102',doer2,
+               '['+os.path.join(tst_dir,'{d:%Y}','tst_[A:B]_{d:%Y%m%d}.tar')+']']
         out = check_output(cmd)
         lines = out.splitlines()
         self.assertEqual(len(lines),12,msg='Basic shell expansion with "[A-B]" failed')
@@ -80,14 +103,16 @@ class Test_ShellExpansion_noF(unittest.TestCase):
 
     def test_shell_expansion_Qmark_noF(self):
         """ Test combining date looping and shell parameter expansion  (with '?') """
-        cmd = [exe,'--quiet','20051228','20060102',doer2,os.path.join(tst_dir,'{d:%Y}','tst_?_{d:%Y%m%d}.tar')]
+        cmd = [exe,'--quiet','20051228','20060102',doer2,
+               '['+os.path.join(tst_dir,'{d:%Y}','tst_?_{d:%Y%m%d}.tar')+']']
         out = check_output(cmd)
         lines = out.splitlines()
         self.assertEqual(len(lines),18,msg='Basic shell expansion with "?" failed')
 
     def test_shell_expansion_star_noF(self):
         """ Test combining date looping and shell parameter expansion  (with '*') """
-        cmd = [exe,'--quiet','20051228','20060102',doer2,os.path.join(tst_dir,'{d:%Y}','tst_*_{d:%Y%m%d}.tar')]
+        cmd = [exe,'--quiet','20051228','20060102',doer2,
+               '['+os.path.join(tst_dir,'{d:%Y}','tst_*_{d:%Y%m%d}.tar')+']']
         out = check_output(cmd)
         lines = out.splitlines()
         self.assertEqual(len(lines),20,msg='Basic shell expansion with "*" failed')
