e164d9cb27e5f65c01c86e62ad2af5ba26b2bb38
Thomas Lavergne <thomas.lavergne@met.no>
2016-01-07 14:39:44 +0100

    add some tests for running outside an ENVIRONEMNT (e.g. from crontab)

--- test/test_basic.py
+++ test/test_basic.py
@@ -20,11 +20,16 @@ class Test_Basic(unittest.TestCase):
         self.assertNotEqual(p.returncode,0,msg='This run should have failed!')
 
     def test_echo_doer(self):
-        """ Test basic date looping with 1-day step """
+        """ Test basic date looping with 1-day step (with and without ENV) """
         cmd = [exe,'--quiet','20040225','20040303','echo','{d:%Y%m%d}']
         out = check_output(cmd)
         lines = out.splitlines()
         self.assertEqual(len(lines),8,msg='Basic date looping failed!')
+        cmd2 = ['env','-i',]
+        cmd2.extend(cmd,)
+        out = check_output(cmd2)
+        lines = out.splitlines()
+        self.assertEqual(len(lines),8,msg='Basic date looping failed (when ENV is empty)!')
 
     def test_quiet(self):
         """ Test the --quiet flag """
@@ -34,6 +39,13 @@ class Test_Basic(unittest.TestCase):
         self.assertEqual(len(lines),1,msg='The --quiet flag does not work (got {} lines of output)'.format(len(lines)))
         self.assertEqual(str(date.today().year),lines[0],
                          msg='The --quiet flag does not work as expected')
+
+    def test_call_noENV(self):
+        """ Test we can call the script in a 'crontab-like' environment (missing ENV) """
+        cmd = ['env','-i',exe,'-v']
+        out = check_output(cmd,stderr=STDOUT)
+        self.assertEqual('loop_wrapper',out.split(' ')[0],
+                         msg='Did not manage to system call to loop_wrapper')
 
     def test_call(self):
         """ Test we can call the binary and retrieve some stdout/sterr text """
