bb1a6871d1733f0bb6a6a0043a2a9d4b080417da
Thomas Lavergne <thomas.lavergne@met.no>
2020-04-17 14:32:05 +0200

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="loop_wrapper",
+    version="3.5.0",
+    author="Thomas Lavergne",
+    author_email="thomas.lavergne@met.no",
+    description="A job-control tool to iterate scripts on ranges of dates.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/metno/loop_wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.6',
+)
